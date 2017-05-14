e5c4656b1c78535de719fbcf047c17aacfec8a32
Vojtech Vladyka <vojtech.vladyka@gmail.com>
2017-05-14 12:27:19 +0200

    Upload script

---
+++ upload.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+scp udf-samples.tar.xz rain@pac.chapadla.cz:/home/rain/public_html/udffsck-test-samples/
+scp udf-samples-extra.tar.xz rain@pac.chapadla.cz:/home/rain/public_html/udffsck-test-samples/
